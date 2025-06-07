1d4134bf5266ace161d5fd4d4e883f1870075981
MATHEUS-ROSA-UFMA <matheus.rosa@discente.ufma.br>
2025-06-07 20:27:29 -0300

    incluir stack c

--- interpret.c
+++ interpret.c
@@ -11,4 +11,6 @@ void interpret (const char *source) {
     sscanf (source, "%s%s", op, arg);
     printf("operação: %s\n", op);
     printf("argumento: %s\n",  arg);
+
+    if (strcmp(op,"push"))
 }
